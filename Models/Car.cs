5f199e39cbbc5a10e3b2a506d9e78b65ff147d38
oluwafemi13 <sundayjabikem@gmail.com>
2022-10-02 23:13:48 +0100

    Added User

--- Models/Car.cs
+++ Models/Car.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Car_Listing_MAUI_Application.Models
 {
-    public class Car
+    public class Car:User
     {
-        public int Id { get; set; }
-        public Guid userId = Guid.NewGuid();
+        //public int Id { get; set; }
+        //public Guid userId = Guid.NewGuid();
         public string Make { get; set; }
         public string Model { get; set; }
         public string Vin { get; set; }
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using Java.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Listing_MAUI_Application.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateCreated { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
